1929b1df27d84e3f4f1247a4419f2d0ade820477
Allan Moreira <114615758+allan-moreira@users.noreply.github.com>
2024-09-29 16:43:25 -0300

    Add files via upload

--- Texto_Rotativo/Contador_3bits/Contador_3bits.v
+++ Texto_Rotativo/Contador_3bits/Contador_3bits.v
@@ -37,22 +37,21 @@
 `timescale 1 ps / 1 ps
 // synopsys translate_on
 module Contador_3bits (
+	aclr,
 	clock,
-	sclr,
 	q);
 
+	input	  aclr;
 	input	  clock;
-	input	  sclr;
 	output	[2:0]  q;
 
 	wire [2:0] sub_wire0;
 	wire [2:0] q = sub_wire0[2:0];
 
 	lpm_counter	lpm_counter_component (
-				.sclr (sclr),
+				.aclr (aclr),
 				.clock (clock),
 				.q (sub_wire0),
-				.aclr (1'b0),
 				.aload (1'b0),
 				.aset (1'b0),
 				.cin (1'b1),
@@ -61,6 +60,7 @@ module Contador_3bits (
 				.cout (),
 				.data ({3{1'b0}}),
 				.eq (),
+				.sclr (1'b0),
 				.sload (1'b0),
 				.sset (1'b0),
 				.updown (1'b1));
@@ -76,7 +76,7 @@ endmodule
 // ============================================================
 // CNX file retrieval info
 // ============================================================
-// Retrieval info: PRIVATE: ACLR NUMERIC "0"
+// Retrieval info: PRIVATE: ACLR NUMERIC "1"
 // Retrieval info: PRIVATE: ALOAD NUMERIC "0"
 // Retrieval info: PRIVATE: ASET NUMERIC "0"
 // Retrieval info: PRIVATE: ASET_ALL1 NUMERIC "1"
@@ -85,10 +85,10 @@ endmodule
 // Retrieval info: PRIVATE: CarryIn NUMERIC "0"
 // Retrieval info: PRIVATE: CarryOut NUMERIC "0"
 // Retrieval info: PRIVATE: Direction NUMERIC "0"
-// Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
+// Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Stratix II"
 // Retrieval info: PRIVATE: ModulusCounter NUMERIC "0"
 // Retrieval info: PRIVATE: ModulusValue NUMERIC "0"
-// Retrieval info: PRIVATE: SCLR NUMERIC "1"
+// Retrieval info: PRIVATE: SCLR NUMERIC "0"
 // Retrieval info: PRIVATE: SLOAD NUMERIC "0"
 // Retrieval info: PRIVATE: SSET NUMERIC "0"
 // Retrieval info: PRIVATE: SSET_ALL1 NUMERIC "1"
@@ -98,12 +98,12 @@ endmodule
 // Retrieval info: CONSTANT: LPM_PORT_UPDOWN STRING "PORT_UNUSED"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_COUNTER"
 // Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "3"
+// Retrieval info: USED_PORT: aclr 0 0 0 0 INPUT NODEFVAL aclr
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT NODEFVAL clock
 // Retrieval info: USED_PORT: q 0 0 3 0 OUTPUT NODEFVAL q[2..0]
-// Retrieval info: USED_PORT: sclr 0 0 0 0 INPUT NODEFVAL sclr
 // Retrieval info: CONNECT: @clock 0 0 0 0 clock 0 0 0 0
 // Retrieval info: CONNECT: q 0 0 3 0 @q 0 0 3 0
-// Retrieval info: CONNECT: @sclr 0 0 0 0 sclr 0 0 0 0
+// Retrieval info: CONNECT: @aclr 0 0 0 0 aclr 0 0 0 0
 // Retrieval info: LIBRARY: lpm lpm.lpm_components.all
 // Retrieval info: GEN_FILE: TYPE_NORMAL Contador_3bits.v TRUE
 // Retrieval info: GEN_FILE: TYPE_NORMAL Contador_3bits.inc FALSE
--- Texto_Rotativo/Contador_3bits/Contador_3bits_bb.v
+++ Texto_Rotativo/Contador_3bits/Contador_3bits_bb.v
@@ -32,12 +32,12 @@
 //applicable agreement for further details.
 
 module Contador_3bits (
+	aclr,
 	clock,
-	sclr,
 	q);
 
+	input	  aclr;
 	input	  clock;
-	input	  sclr;
 	output	[2:0]  q;
 
 endmodule
@@ -45,7 +45,7 @@ endmodule
 // ============================================================
 // CNX file retrieval info
 // ============================================================
-// Retrieval info: PRIVATE: ACLR NUMERIC "0"
+// Retrieval info: PRIVATE: ACLR NUMERIC "1"
 // Retrieval info: PRIVATE: ALOAD NUMERIC "0"
 // Retrieval info: PRIVATE: ASET NUMERIC "0"
 // Retrieval info: PRIVATE: ASET_ALL1 NUMERIC "1"
@@ -54,10 +54,10 @@ endmodule
 // Retrieval info: PRIVATE: CarryIn NUMERIC "0"
 // Retrieval info: PRIVATE: CarryOut NUMERIC "0"
 // Retrieval info: PRIVATE: Direction NUMERIC "0"
-// Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
+// Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Stratix II"
 // Retrieval info: PRIVATE: ModulusCounter NUMERIC "0"
 // Retrieval info: PRIVATE: ModulusValue NUMERIC "0"
-// Retrieval info: PRIVATE: SCLR NUMERIC "1"
+// Retrieval info: PRIVATE: SCLR NUMERIC "0"
 // Retrieval info: PRIVATE: SLOAD NUMERIC "0"
 // Retrieval info: PRIVATE: SSET NUMERIC "0"
 // Retrieval info: PRIVATE: SSET_ALL1 NUMERIC "1"
@@ -67,12 +67,12 @@ endmodule
 // Retrieval info: CONSTANT: LPM_PORT_UPDOWN STRING "PORT_UNUSED"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_COUNTER"
 // Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "3"
+// Retrieval info: USED_PORT: aclr 0 0 0 0 INPUT NODEFVAL aclr
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT NODEFVAL clock
 // Retrieval info: USED_PORT: q 0 0 3 0 OUTPUT NODEFVAL q[2..0]
-// Retrieval info: USED_PORT: sclr 0 0 0 0 INPUT NODEFVAL sclr
 // Retrieval info: CONNECT: @clock 0 0 0 0 clock 0 0 0 0
 // Retrieval info: CONNECT: q 0 0 3 0 @q 0 0 3 0
-// Retrieval info: CONNECT: @sclr 0 0 0 0 sclr 0 0 0 0
+// Retrieval info: CONNECT: @aclr 0 0 0 0 aclr 0 0 0 0
 // Retrieval info: LIBRARY: lpm lpm.lpm_components.all
 // Retrieval info: GEN_FILE: TYPE_NORMAL Contador_3bits.v TRUE
 // Retrieval info: GEN_FILE: TYPE_NORMAL Contador_3bits.inc FALSE
